383addcc45d4c62550da4eebb277e1eb5e5c5b95
许晓周 <xxz@eventist.cn>
2018-07-09 20:15:01 +0800

    init

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,28 @@
 module.exports = {
   presets: [
     '@vue/app',
-    ['es2015',
-      {
-        'modules': false
-      }
-    ]
+    // [
+    //   'es2015',
+    //   {
+    //     'modules': false
+    //   }
+    // ],
   ],
   plugins: [
     'syntax-dynamic-import',
-    'component',
-    {
-      'libraryName': 'element-ui',
-      'styleLibraryName': 'theme-chalk'
-    }
+    [
+      'component',
+      {
+        'libraryName': 'element-ui',
+        'styleLibraryName': 'theme-chalk'
+      }
+    ]
+    // [
+    //   'component',
+    //   {
+    //     'libraryName': 'element-ui',
+    //     'styleLibraryName': 'theme-chalk'
+    //   }
+    // ]
   ]
 }
